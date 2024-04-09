86c1f323ba57d46aeb9b48a2a7543c9192518546
Michael <159481999+OdeyLlinusMichael@users.noreply.github.com>
2024-04-09 17:04:53 +0100

    Update About2.jsx

--- portfolio/src/components/About2/About2.jsx
+++ portfolio/src/components/About2/About2.jsx
@@ -25,13 +25,24 @@ const About2 = () => {
     return (
         <section className="runa" ref={ref}>
             <div className="row-gy-5">
-                <motion.div className="col-lg-7" initial={{ y: 1000 }} animate={{ y: isVisible ? 10 : 1000 }} transition={{ duration: 1 }} >
+                <motion.div className="col-lg-7" 
+                    initial={{ opacity: 0, y: 200 }} 
+                    animate={{ opacity: isVisible ? 1 : 0, y: isVisible ? 10 : 200 }} 
+                    transition={{ duration: 1 }} 
+                    ref={ref}
+                >
                     <h2 className="text-7">I'm <span className="text-primary">Odey Linus Michael</span></h2>
                     <p className="txt3">With six years of professional experience spanning across graphics, product, and UI/UX design, I approach every project with a blend of creativity and strategic insight. My journey in the design realm has equipped me with the expertise to craft visually stunning solutions that not only meet but exceed client expectations. From conceptualization to execution, I thrive on the challenge of bringing ideas to life through thoughtful design that resonates with users on a deep level.</p>
                     <p>Outside the design studio, I find solace in the world of cinema, where storytelling and visual aesthetics intertwine to create immersive experiences. Whether it’s a classic film or the latest blockbuster, I draw inspiration from the diverse narratives and visual styles, which in turn fuel my creativity and enrich my approach to design. Explore my portfolio to witness how my passion for design and love for movies converge, shaping unique and engaging user experiences.</p>
                 </motion.div>
 
-                <motion.div className="col-lg-5 col-xl-4" initial={{ y: 1000 }} animate={{ y: isVisible ? 10 : 1000 }} transition={{ duration: 1 }} >
+                <motion.div className="col-lg-5 col-xl-4"
+                    initial={{ opacity: 0, y: 200 }} 
+                    animate={{ opacity: isVisible ? 1 : 0, y: isVisible ? 10 : 200 }} 
+                    transition={{ duration: 1 }} 
+                    ref={ref}
+                >
+                </motion.div>
                     <div className="ps-lg-4">
                         <ul className="list-style-2">
                             <li>Name:<span className="fw-600 me-2">Odey Linus Michael</span></li>
