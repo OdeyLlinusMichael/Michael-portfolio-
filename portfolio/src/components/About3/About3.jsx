f3206377787287491a4c6f7d4360af68a363bf68
Michael <159481999+OdeyLlinusMichael@users.noreply.github.com>
2024-04-09 13:39:32 +0100

    Update About3.jsx

--- portfolio/src/components/About3/About3.jsx
+++ portfolio/src/components/About3/About3.jsx
@@ -23,15 +23,15 @@ const About3 = () => {
     return (
         <section className="runa">
             <div className="brands-grid separator-border mt-5">
-                <motion.div className="row" initial={{ y: 1000 }} animate={{ y: isVisible ? 10 : 1000 }} transition={{ duration: 1 }} >
-                    <div className="col-6 col-md-3">
+                <div className="row">
+                    <motion.div className="col-6 col-md-3" initial={{ y: 1000 }} animate={{ y: isVisible ? 10 : 1000 }} transition={{ duration: 1 }} >
                         <div className="featured-box text-center">
                             <svg xmlns="http://www.w3.org/2000/svg" height="50" width="50" viewBox="0 0 512 512"><path fill="#D5D8DC" d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96H64C28.7 96 0 124.7 0 160v96H192 320 512V160c0-35.3-28.7-64-64-64H384V56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM512 288H320v32c0 17.7-14.3 32-32 32H224c-17.7 0-32-14.3-32-32V288H0V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V288z"/></svg>
                             <i className="fa-solid fa-briefcase fa-2xl"></i>
                             <h4 className="text-12 text-muted mb-0"><span className="counter" data-from="0" data-to="10">6</span>+</h4>
                             <p className="mb-0">Years Experience</p>
                         </div>
-                    </div>
+                    </motion.div>
                     <div className="col-6 col-md-3">
                         <div className="featured-box text-center"> 
                             <svg xmlns="http://www.w3.org/2000/svg" height="50" width="50" viewBox="0 0 512 512"><path fill="#D5D8DC" d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>
@@ -46,7 +46,7 @@ const About3 = () => {
                             <p className="mb-0">Projects Done</p>
                         </div>
                     </div>
-                </motion.div>
+                </div>
             </div>
         </section>
     );
