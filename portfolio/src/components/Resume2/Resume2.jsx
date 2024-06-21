cd5b4ca13653e00049fc1c949eeca83cf59e5a23
Michael <159481999+OdeyLlinusMichael@users.noreply.github.com>
2024-06-21 18:07:49 +0000

    new3

--- portfolio/src/components/Resume2/Resume2.jsx
+++ portfolio/src/components/Resume2/Resume2.jsx
@@ -35,29 +35,37 @@ const Resume2 = () => {
                                <p>I earned a certificate of completion in Figma UI/UX Design Advanced from Udemy, Inc., further expanding my skill set and expertise in user interface and experience design.</p>
                             </div>
                         </div>
-                        <div className="boxx">
-                            <h4>2018 - 2023</h4>
-                            <h3>Federal Polytechnic Auchi, Edo State</h3>
-                            <p>Diploma in Banking and Finance</p>
-                            <p>At Federal Polytechnic Auchi, Edo State, I pursued a Diploma in Banking and Finance, gaining valuable insights into financial principles and management practices.</p>
+                        <div className="box1">
+                            <div className="boxx">
+                               <h4>2018 - 2023</h4>
+                               <h3>Federal Polytechnic Auchi, Edo State</h3>
+                               <p>Diploma in Banking and Finance</p>
+                               <p>At Federal Polytechnic Auchi, Edo State, I pursued a Diploma in Banking and Finance, gaining valuable insights into financial principles and management practices.</p>
+                            </div>
                         </div>
-                        <div className="boxx">
-                            <h4>2016 - 2018</h4>
-                            <h3>Global-Tech World Computer Institute</h3>
-                            <p>Desktop Publishing and graphic designs</p>
-                            <p>At Global-Tech World Computer Institute, I specialized in desktop publishing and graphic design, honing my skills and knowledge in these areas to prepare for a career in the creative industry.</p>
+                        <div className="box1">
+                            <div className="boxx">
+                                <h4>2016 - 2018</h4>
+                                <h3>Global-Tech World Computer Institute</h3>
+                                <p>Desktop Publishing and graphic designs</p>
+                                <p>At Global-Tech World Computer Institute, I specialized in desktop publishing and graphic design, honing my skills and knowledge in these areas to prepare for a career in the creative industry.</p>
+                            </div>
                         </div>
-                        <div className="boxx">
-                            <h4>2010 - 2016</h4>
-                            <h3>Hephzibah college secondary school</h3>
-                            <p>Secondary School Leacing Certificate(SSCE)</p>
-                            <p>My secondary school education provided me with a solid academic grounding and further fueled my passion for exploration and knowledge acquisition.</p>
+                        <div className="box1">
+                            <div className="boxx">
+                               <h4>2010 - 2016</h4>
+                               <h3>Hephzibah college secondary school</h3>
+                               <p>Secondary School Leacing Certificate(SSCE)</p>
+                               <p>My secondary school education provided me with a solid academic grounding and further fueled my passion for exploration and knowledge acquisition.</p>
+                            </div>
                         </div>
-                        <div className="boxx">
-                            <h4>2000 - 2010</h4>
-                            <h3>First Gate Primary School</h3>
-                            <p>Primary School Leaving Certificate</p>
-                            <p>During my primary school years, I began cultivating a curiosity for learning and laying the foundation for my future endeavors.</p>
+                        <div className="box1">
+                            <div className="boxx">
+                               <h4>2000 - 2010</h4>
+                               <h3>First Gate Primary School</h3>
+                               <p>Primary School Leaving Certificate</p>
+                               <p>During my primary school years, I began cultivating a curiosity for learning and laying the foundation for my future endeavors.</p>
+                            </div>
                         </div>
                     </div>
                 </section>
@@ -69,20 +77,26 @@ const Resume2 = () => {
                         <h2>EXPERIENCE</h2>
                     </header>
                     <div className="contentsx">
-                        <div className="boxx">
-                            <h4>2021 - till date</h4>
-                            <h3>Interior Product designer at Lyn’s Decor</h3>
-                            <p>As an Interior Product Designer at Lyn’s Decor, I had the opportunity to merge my passion for design with the intricacies of interior spaces. Collaborating closely with clients and the design team, I conceptualized and developed innovative product designs that seamlessly integrated with diverse interior aesthetics. From conceptualization to execution, I contributed to creating unique and functional interior products that enhanced the ambiance and functionality of various spaces.</p>
+                       <div className="box1">
+                           <div className="boxx">
+                               <h4>2021 - till date</h4>
+                               <h3>Interior Product designer at Lyn’s Decor</h3>
+                               <p>As an Interior Product Designer at Lyn’s Decor, I had the opportunity to merge my passion for design with the intricacies of interior spaces. Collaborating closely with clients and the design team, I conceptualized and developed innovative product designs that seamlessly integrated with diverse interior aesthetics. From conceptualization to execution, I contributed to creating unique and functional interior products that enhanced the ambiance and functionality of various spaces.</p>
+                            </div>
                         </div>
-                        <div className="boxx">
-                            <h4>2019 - 2020</h4>
-                            <h3>Product designer at Debonairs Pizza Nigeria</h3>
-                            <p>As a Product Designer at Debonairs Pizza Nigeria, I spearheaded the conceptualization and development of new menu items and packaging designs. Collaborating with cross-functional teams, I ensured that our products not only met but exceeded customer expectations, enhancing the overall dining experience.</p>
+                        <div className="box1">
+                            <div className="boxx">
+                               <h4>2019 - 2020</h4>
+                               <h3>Product designer at Debonairs Pizza Nigeria</h3>
+                               <p>As a Product Designer at Debonairs Pizza Nigeria, I spearheaded the conceptualization and development of new menu items and packaging designs. Collaborating with cross-functional teams, I ensured that our products not only met but exceeded customer expectations, enhancing the overall dining experience.</p>
+                            </div>
                         </div>
-                        <div className="boxx">
-                            <h4>2018 - 2019</h4>
-                            <h3>Graphics designer at Achievers LLc</h3>
-                            <p>As a Graphics Designer at Achievers LLC, I was responsible for creating visually compelling designs for various marketing materials and digital platforms. My role involved conceptualizing and executing innovative design solutions that effectively communicated the company's brand identity and messaging to target audiences.</p>
+                        <div className="box1">
+                            <div className="boxx">
+                               <h4>2018 - 2019</h4>
+                              <h3>Graphics designer at Achievers LLc</h3>
+                               <p>As a Graphics Designer at Achievers LLC, I was responsible for creating visually compelling designs for various marketing materials and digital platforms. My role involved conceptualizing and executing innovative design solutions that effectively communicated the company's brand identity and messaging to target audiences.</p>
+                            </div>
                         </div>
                     </div>
                 </section>
