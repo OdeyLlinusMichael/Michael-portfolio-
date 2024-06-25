910dcf2ae5931d61178dae4c9ddb3fc7f78c2bbf
Michael <159481999+OdeyLlinusMichael@users.noreply.github.com>
2024-06-25 00:24:19 +0000

    Google analytics

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-B5VXSLMP78");
+
+ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
